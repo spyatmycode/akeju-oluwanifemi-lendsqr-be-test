edb9f60ed3abfaf463dac8fc430d4df4a9d74f35
Oluwanifemi Akeju <akejunifemi11@gmail.com>
2025-06-11 07:54:07 +0100

    feat(): Welcom home route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({})
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { errorHandler } from './middleware/error';
 import userRoutes from './controllers/userController';
 import walletRoutes from './controllers/walletController';
@@ -8,6 +8,9 @@ import { authMiddleware } from './middleware/auth';
 
 const app = express();
 app.use(express.json());
+app.use('/', (req: Request, res: Response) => { 
+    res.status(200).json({ message: "Welcome to Oluwanifemi's Lendsqr Backend Assessment" }) 
+})
 app.use('/users', userRoutes);
 app.use('/wallets', authMiddleware, walletRoutes);
 app.use(errorHandler);
